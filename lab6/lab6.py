787743bd7d45bb3e990e4640266e7aead806d053
MrSkl1f <sklifden@mail.ru>
2020-12-08 13:41:25 +0300

    Add 6 lab

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -28,10 +28,40 @@ def RequestWithJoin():
     print('Name: ' + result[0] + ', team: ' + result[1] + ', general director: ' + result[2])
 
 # 3. Выполнить запрос с ОТВ(CTE) и оконными функциями
+def Request3():
+    request = '''
+        select cur.player_country as "country", avg(cur.player_height) over (partition by player_country) as "height"
+        from (
+            select player_country, player_height  
+            from players
+            where player_country = 'Canada'
+        ) as cur
+        union
+        select cur.player_country as "country", avg(cur.player_height) over (partition by player_country) as "height"
+        from (
+            select player_country, player_height  
+            from players
+            where player_country = 'United States'
+        ) as cur;
+    '''
+    cursor.execute(request)
+    for row in cursor:
+        print('Country: ' + row[0] + ', average height: ' + str(row[1]))
 
-
-# 4. 
-
+# 4. Выполнить запрос к метаданным;
+def RequestToMetaData():
+    request = '''
+    select pp.proname 
+    from pg_catalog.pg_proc pp
+    where pp."oid" = (
+        select pt.tgfoid
+        from pg_catalog.pg_trigger pt 
+        where pt.tgname = 'draft'
+    );
+    '''
+    cursor.execute(request)
+    print(cursor.fetchall()[0][0])
+    
 # 5. Вызвать скалярную функцию
 # select * from skalar_func();
 def ScalarFunc():
@@ -51,10 +81,40 @@ def TableFunc():
             + str(row[7]) + ', country: ' + row[8])
 
 # 7. Вызвать хранимую процедуру
+#Вызвать хранимую процедуру (написанную в третьей лабораторной работе)
+def CallStoredProcedure():
+    request = "call get_trigger_and_func(cast('draft' as name));"
+    cursor.execute(request)
+    print(conn.notices[-1])
+
+# 8. Вызвать системную функцию или процедуру
+def CallSystemFunc():
+    request = 'select version()'
+    cursor.execute(request)
+    print(cursor.fetchall()[0][0])
+
+# 9. Создатьтаблицувбазеданных, соответствующую тематике БД
+def CreateTable():
+    request = '''
+    drop table if exists drafts;
+    create table if not exists drafts (
+        id serial not null,
+        player_id int,
+        last_team_id int,
+        new_team_id int
+    );
+    '''
+    cursor.execute(request)
+    conn.commit()
 
 
-
-
+def Insert(id, last_id, new_id):
+    request = f'''
+    insert into drafts(player_id, last_team_id, new_team_id)
+    values({id}, {last_id}, {new_id})
+    '''
+    cursor.execute(request)
+    conn.commit()
 
 cursor.close()
 conn.close()
